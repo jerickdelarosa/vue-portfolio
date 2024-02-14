c396861d79baf48138cf8878b27744f6c2071247
Jerick Dela Rosa <delarosajerick26@gmail.com>
2024-02-14 13:31:36 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <router-view name="navigation" />
+  <div class="container px-2 py-4">
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+/* NAVIGATION */
+import MainNav from '@/components/navigation/MainNav.vue'
+/* COMPONENTS */
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      components: {
+        default: HomeView,
+        navigation: MainNav,
+      },
+      meta: {
+        title: 'Jerick Dela Rosa',
+      }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+      meta: {
+        title: 'About',
+      }
+    }
+  ],
+  
+})
+
+router.beforeEach((to, from ,next) => {
+  document.title = `${to.meta.title} | Online Portfolio`
+
+  next()
+
+})
+
+export default router

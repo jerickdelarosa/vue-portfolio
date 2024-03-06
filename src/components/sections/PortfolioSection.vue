37794c1ce07908a4c196adae34cb6dd96cd8d95d
Jerick Dela Rosa <delarosajerick26@gmail.com>
2024-03-06 10:13:46 +0800

    follow up update

--- src/components/sections/PortfolioSection.vue
+++ src/components/sections/PortfolioSection.vue
@@ -12,8 +12,17 @@
 
             <div class="py-16">  
                 <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+    
+
+
+
+
+
+
+
+
                     <div :class="`space-y-6 lg:space-y-0 lg:flex lg:items-center lg:gap-12 ${(projectList.length - 1) > index ? 'mb-20' : ''}  text-center lg:text-start`" v-for="(project, index) in projectList" :key="`project-${index}`">
-                        <div :class="`md:6/12 lg:w-6/12 ${(index %= 2) == 0 ? 'order-1 lg:order-2' : ''} flex justify-center`">
+                        <div :class="`md:6/12 lg:w-6/12 ${(index %= 2) == 0 ? 'order-1 lg:order-2' : ''} flex justify-center hover:opacity-75 cursor-pointer`">
                             <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="project-img" loading="lazy" class="rounded-lg h-auto md:w-[28rem] lg:w-auto">
                         </div>
                         
@@ -25,6 +34,10 @@
                             </div>
                         </div>
                     </div>
+
+
+
+
                 </div>
             </div>
 

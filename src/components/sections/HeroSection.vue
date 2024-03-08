2b1ee2b63cb32da6007eb20fa44a9f7802c81b29
Jerick Dela Rosa <delarosajerick26@gmail.com>
2024-03-08 14:32:22 +0800

    initial elements for hero

--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -1,9 +1,28 @@
-
-import { SparklesIcon } from '@heroicons/vue/24/outline';
 <template>
     <section id="home" class="bg-[#f9f9f9] antialiased">
-        <div class="h-[100vh] w-100 flex justify-center items-center">
-                <span class="text-[50px] sm:text-[100px] font-semibold">Hero / home</span>
+        <div class="max-w-screen-xl px-4 pt-8 mx-auto lg:px-6 sm:pt-16 lg:pt-24">
+            <div class="container mx-auto flex md:px-24 md:pt-24 md:pb-8 md:flex-row flex-col items-center">
+                <div class="lg:max-w-lg lg:w-full mb-5 md:mb-0 md:w-1/2 w-3/6 md:order-2">
+                    <img class="object-cover object-center rounded-full" alt="hero_img" src="https://jerickdelarosa.vercel.app/assets/jedero-62cde2ee.jpg">
+                </div>
+                <div class="lg:flex-grow mt-12 md:mt-0 md:w-1.5/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center md:order-1">
+                    <h1 class="text-2xl font-extrabold leading-9 tracking-tight mb-3 text-gray-900 sm:text-4xl sm:leading-10 md:text-5xl md:leading-normal">
+                        Jerick Dela Rosa
+                    </h1>
+                    <p class="mb-8 md:pl-0  pl-2 pr-2 leading-relaxed">
+                        Short description here, Short description here Short description here Short description here Short
+                        description here Short description here.
+                    </p>
+                    <div class="flex justify-center">
+                        <a href="#" class="inline-flex text-white bg-emerald-600 border-0 py-2 px-6 focus:outline-none hover:bg-emerald-600 rounded text-lg">Start
+                            Exploring
+                        </a>
+                        <a href="#" class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Read
+                            articles</a>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     </section>
 </template>
--- src/components/sections/PortfolioSection.vue
+++ src/components/sections/PortfolioSection.vue
@@ -22,25 +22,52 @@
 
 
                     <div :class="`space-y-6 lg:space-y-0 lg:flex lg:items-center lg:gap-12 ${(projectList.length - 1) > index ? 'mb-20' : ''}  text-center lg:text-start`" v-for="(project, index) in projectList" :key="`project-${index}`">
-                        <div :class="`md:6/12 lg:w-6/12 ${(index %= 2) == 0 ? 'order-1 lg:order-2' : ''} flex justify-center hover:opacity-75 cursor-pointer`">
+                        <div :class="`lg:w-6/12 ${(index %= 2) == 0 ? 'order-1 lg:order-2' : ''} flex justify-center`">
                             <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="project-img" loading="lazy" class="rounded-lg h-auto md:w-[28rem] lg:w-auto">
                         </div>
                         
-                        <div :class="`md:6/12 lg:w-6/12 ${(index %= 2) == 0 ? 'order-2 lg:order-1' : ''}`">
+                        <div :class="`lg:w-6/12 ${(index %= 2) == 0 ? 'order-2 lg:order-1' : ''}`">
                             <h1 class="font-extrabold uppercase text-2xl mb-5 text-[#2d2e32]">{{ project.title }}</h1>
                             <p class="mt-2 text-gray-600 md:max-w-[32rem] mx-auto lg:mx-0"> {{ project.description }} </p>
                             <div class="mt-6 flex justify-center lg:justify-start gap-1">
                                 <span v-for="stack in project.tech" :key="stack.id" class="inline-flex items-center rounded-md bg-[#0E6CC2] px-2 py-1 text-xs text-white font-extrabold tracking-widest">{{ stack.name }}</span>
                             </div>
+
+
+                            <!-- <div class="mt-4 flex flex-col items-center gap-y-4 sm:flex-row sm:gap-x-8 flex-wrap">
+                                <div class="relative flex items-center gap-x-4 text-start">
+                                    <img src="https://noticias.animeonegai.com/wp-content/uploads/2023/12/Sousou-no-Frieren-tamano-atributos-fern-miniatura.webp" alt="" class="h-10 w-10 rounded-full bg-gray-50" />
+                                    <div class="text-sm leading-6">
+                                    <p class="font-semibold text-gray-900">
+                                        Juan Dela Cruz
+                                    </p>
+                                    <p class="text-gray-600">Lead Web Developer</p>
+                                    </div>
+                                </div>
+                                <div class="relative flex items-center gap-x-4 text-start">
+                                    <img src="https://noticias.animeonegai.com/wp-content/uploads/2023/12/Sousou-no-Frieren-tamano-atributos-fern-miniatura.webp" alt="" class="h-10 w-10 rounded-full bg-gray-50" />
+                                    <div class="text-sm leading-6">
+                                    <p class="font-semibold text-gray-900">
+                                        Juan Dela Cruz
+                                    </p>
+                                    <p class="text-gray-600">Lead Web Developer</p>
+                                    </div>
+                                </div>
+                                <div class="relative flex items-center gap-x-4 text-start">
+                                    <img src="https://noticias.animeonegai.com/wp-content/uploads/2023/12/Sousou-no-Frieren-tamano-atributos-fern-miniatura.webp" alt="" class="h-10 w-10 rounded-full bg-gray-50" />
+                                    <div class="text-sm leading-6">
+                                    <p class="font-semibold text-gray-900">
+                                        Juan Dela Cruz
+                                    </p>
+                                    <p class="text-gray-600">Lead Web Developer</p>
+                                    </div>
+                                </div>
+                            </div> -->
+                            
                         </div>
                     </div>
-
-
-
-
                 </div>
             </div>
-
         </div>
     </section>
 </template>

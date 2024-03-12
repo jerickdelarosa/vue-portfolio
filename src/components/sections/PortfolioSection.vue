63a24e6f0d4fd4f5205d1f0d8deb9db8fa4bac83
Jerick Dela Rosa <delarosajerick26@gmail.com>
2024-03-12 14:40:10 +0800

    small update on project image
    - creation of image template

--- src/components/sections/PortfolioSection.vue
+++ src/components/sections/PortfolioSection.vue
@@ -15,9 +15,11 @@
     
 
                     <div :class="`space-y-6 lg:space-y-0 lg:flex lg:items-center lg:gap-12 ${(projectList.length - 1) > index ? 'mb-20' : ''}  text-center lg:text-start`" v-for="(project, index) in projectList" :key="`project-${index}`">
-                        <div :class="`lg:w-6/12 ${(index %= 2) == 0 ? 'order-1 lg:order-2' : ''} flex justify-center bg-slate-200 rounded-lg`">
-                            <!-- <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="project-img" loading="lazy" class="rounded-lg h-auto md:w-[28rem] lg:w-auto"> -->
-                            <img src="../../assets/images/image-frame.png" alt="project-img" loading="lazy" class="rounded-lg h-auto md:w-[16rem] lg:w-auto">
+                        <!-- <div :class="`lg:w-6/12 ${(index %= 2) == 0 ? 'order-1 lg:order-2' : ''} flex justify-center bg-slate-200 rounded-lg`">
+                            <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="project-img" loading="lazy" class="rounded-lg h-auto md:w-[28rem] lg:w-auto">
+                        </div> -->
+                        <div :class="`lg:w-6/12 ${(index %= 2) == 0 ? 'order-1 lg:order-2' : ''} flex justify-center items-center bg-slate-200 rounded-lg h-[12rem] md:h-[20rem]`">
+                            <img src="../../assets/images/image-frame.png" alt="project-img" loading="lazy" class="rounded-lg h-32 w-32">
                         </div>
                         
                         <div :class="`lg:w-6/12 ${(index %= 2) == 0 ? 'order-2 lg:order-1' : ''}`">
